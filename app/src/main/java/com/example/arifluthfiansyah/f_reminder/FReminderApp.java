7d747105714d3adc2de54204c0ab26c234bef114
Muhammad Iqbal Kurniawan <m.kurniawanibal@gmail.com>
2018-04-13 19:14:15 +0700

    Data Architecture

--- app/src/main/java/com/example/arifluthfiansyah/f_reminder/FReminderApp.java
+++ app/src/main/java/com/example/arifluthfiansyah/f_reminder/FReminderApp.java
@@ -1,6 +1,13 @@
 package com.example.arifluthfiansyah.f_reminder;
 
 import android.app.Application;
+import android.content.Context;
+
+import com.example.arifluthfiansyah.f_reminder.data.DataManager;
+import com.example.arifluthfiansyah.f_reminder.di.component.ApplicationComponent;
+import com.example.arifluthfiansyah.f_reminder.di.module.ApplicationModule;
+
+import javax.inject.Inject;
 
 import io.realm.Realm;
 
@@ -9,10 +16,25 @@ import io.realm.Realm;
  */
 
 public class FReminderApp extends Application {
+    @Inject
+    DataManager mDataManager;
 
+    private ApplicationComponent mApplicationComponent;
+
+    public static FReminderApp get(Context context){
+        return (FReminderApp) context.getApplicationContext();
+    }
     @Override
     public void onCreate() {
         super.onCreate();
         Realm.init(this);
+        mApplicationComponent = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this)).build();
+
+        mApplicationComponent.inject(this);
+    }
+
+    public ApplicationComponent getmApplicationComponent() {
+        return mApplicationComponent;
     }
 }
